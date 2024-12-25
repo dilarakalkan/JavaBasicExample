62a37fda22c7b4350b0f34917ac9d8c4e66f9488
dilarakalkan <dilarakalkanxx@gmail.com>
2024-12-25 11:25:22 +0300

    algoritmalar için temel

--- src/main/java/org/example/algorithms/PrimeChecker.java
+++ src/main/java/org/example/algorithms/PrimeChecker.java
@@ -2,29 +2,39 @@ package org.example.algorithms;
 
 import java.util.Scanner;
 
-//asal sayı kontrolü yapmak için kullanılan algoritma
+// Asal sayı kontrolü yapmak için kullanılan algoritma
 public class PrimeChecker {
-    public static void main(String[]args){
-        Scanner scanner=new Scanner(System.in);
-        int num= scanner.nextInt();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        boolean isPrime=true;
-        if (num<=1) {
-            isPrime = false;
-        }
-        else{
-            for(int i=2; i<=Math.sqrt(num); i++){
-                if(num% i ==0){
-                    isPrime=false;
-                    break;
+        System.out.println("Bir sayı girin: ");
+
+        // Kullanıcıdan geçerli bir giriş alındığından emin olun
+        if (scanner.hasNextInt()) {
+            int num = scanner.nextInt();
+
+            boolean isPrime = true;
+
+            if (num <= 1) {
+                isPrime = false;
+            } else {
+                for (int i = 2; i <= Math.sqrt(num); i++) {
+                    if (num % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
                 }
             }
-        }
-if(isPrime){
-    System.out.println(num+ " bir asal sayıdır");
 
-}else{
-    System.out.println(num+ "asal sayı değildir");
-}
+            if (isPrime) {
+                System.out.println(num + " bir asal sayıdır.");
+            } else {
+                System.out.println(num + " asal sayı değildir.");
+            }
+        } else {
+            System.out.println("Lütfen geçerli bir tamsayı girin.");
+        }
+
+        scanner.close(); // Scanner'ı kapatmayı unutmayın
     }
 }
